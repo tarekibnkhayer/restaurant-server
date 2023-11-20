dc5bbfa175f62eec2057278448dd6944708a2c89
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-11-20 12:56:14 +0600

    create api and middleware for checking admin

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 2626;
+const { ObjectId } = require('mongodb');
 
 // middleware:
 app.use(express.json());
@@ -11,6 +13,7 @@ app.use(cors({
   credentials: true
 }));
 
+ 
 
 
 
@@ -34,7 +37,56 @@ async function run() {
     const userCollection = client.db("restaurantDB").collection("users");
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // auth related api:
+    app.post('/jwt', async(req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+      res.send({token});
+    });
 
+    // middleware:
+ const verifyToken = (req, res, next) => {
+  if(!req.headers.authorization){
+    return res.status(401).send({message: 'forbidden'});
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      return res.status(401).send({message: 'forbidden'});
+    }
+    req.decoded = decoded;
+    next()
+  })
+};
+// use verifyAdmin after  verifyToken
+const verifyAdmin = async(req, res, next) => {
+  const email = req.decoded.email;
+  const query = {email: email};
+  const user = await userCollection.findOne(query);
+  const isAdmin = user?.role === 'admin';
+  if(!isAdmin){
+    return res.status(403).send({message: 'forbidden'});
+  }
+  next();
+}
+
+    app.get("/users/admin/:email", verifyToken, verifyAdmin, async(req, res) => {
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'Unauthorized access'});
+      };
+      const query = {email : email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        if(user?.role === 'admin'){
+          admin = true;
+        };
+      };
+      res.send({admin});
+    })
+
+    // 
     app.get("/menu", async(req, res) => {
        const menuIds = req.query.menuIds;
       if(menuIds){
@@ -59,21 +111,49 @@ async function run() {
       const query = {email: email};
       const result = await cartCollection.find(query).toArray();
       res.send(result);
-    })
-    app.post("/carts", async(req, res) => {
+    });
+
+    app.get("/users",verifyToken, verifyAdmin,  async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/carts",verifyToken, async(req, res) => {
       const cart = req.body;
       const result = await cartCollection.insertOne(cart);
       res.send(result);
     });
     app.post("/users", async(req, res) => {
       const user = req.body;
+      const query = {email: user.email}
+      const isExist = await userCollection.findOne(query);
+      if(isExist){
+        return res.send({message: "User Already Exist"});
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
-    app.delete("/carts/:id", async(req, res) => {
+    app.delete("/carts/:id",verifyToken, async(req, res) => {
       const id = req.params.id;
       const query = {menuId: id};
       const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.delete("/users/:id",verifyToken, async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", verifyToken, verifyAdmin, async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      };
+      const result = await userCollection.updateOne(filter, updateDoc)
       res.send(result);
     })
     // Send a ping to confirm a successful connection
